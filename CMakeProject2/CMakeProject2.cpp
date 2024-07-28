ff536a711263808b6e97e5048d3b8f9ef9cf88a1
azarankin <azarankin@deloitte.co.il>
2024-07-28 20:37:31 +0300

    init

---
+++ CMakeProject2/CMakeProject2.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeProject1.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProject2.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake project 2." << endl;
+	return 0;
+}
