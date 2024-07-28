bd569ca224dc078f918c8e3a5704d523c1752ee9
azarankin <azarankin@deloitte.co.il>
2024-07-28 21:16:01 +0300

    init fix Main1 and Main2

---
+++ Main1/main.cpp
@@ -0,0 +1,10 @@
+﻿
+
+#include "main.h"
+
+
+int main()
+{
+	std::cout << "Main1 \n";
+	return 0;
+}
---
+++ Main2/main.cpp
@@ -0,0 +1,10 @@
+﻿
+
+#include "main.h"
+
+
+int main()
+{
+	std::cout << "Main2 \n";
+	return 0;
+}
